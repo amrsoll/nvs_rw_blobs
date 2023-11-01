b4290ee2644c878416463f05c48587e5b4938334
Axel Soll <axel@micro-technic.com>
2023-11-01 15:27:54 +0100

    remove superfluous comments.

--- main/nvs_blob_example_main.c
+++ main/nvs_blob_example_main.c
@@ -22,22 +22,9 @@
 #define STORAGE_BLOB_1 "storage_blob_1"
 #define STORAGE_BLOB_2 "storage_blob_2"
 
-#if CONFIG_IDF_TARGET_ESP32C3
-#define BOOT_MODE_PIN GPIO_NUM_9
-#else
-#define BOOT_MODE_PIN GPIO_NUM_0
-#endif //CONFIG_IDF_TARGET_ESP32C3
-
 #define BUFFER_LEN_1 6000
 #define BUFFER_LEN_2 4000
 
-
-/* Save new run time value in NVS
-   by first reading a table of previously saved values
-   and then adding the new value at the end of the table.
-   Return an error if anything goes wrong
-   during this process.
- */
 esp_err_t save_blobs(void)
 {
     nvs_handle_t my_handle;
@@ -48,10 +35,8 @@ esp_err_t save_blobs(void)
     if (err != ESP_OK) return err;
 
     // Read previously saved blob if available
-    // uint32_t* my_blob = malloc(required_size);
     char * my_blob = malloc(BUFFER_LEN_1);
 
-    // Write value including previously saved blob if available
     printf("Writing : %zu\n", BUFFER_LEN_1 * sizeof(char));
     err = nvs_set_blob(my_handle, STORAGE_BLOB_1, my_blob, BUFFER_LEN_1 * sizeof(char));
     my_blob = realloc(my_blob, BUFFER_LEN_2 * sizeof(char));
@@ -69,11 +54,6 @@ esp_err_t save_blobs(void)
     return ESP_OK;
 }
 
-/* Read from NVS and print restart counter
-   and the table with run times.
-   Return an error if anything goes wrong
-   during this process.
- */
 esp_err_t read_blob_size(void)
 {
     nvs_handle_t my_handle;
@@ -83,13 +63,8 @@ esp_err_t read_blob_size(void)
     err = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &my_handle);
     if (err != ESP_OK) return err;
 
-    // Read restart counter
-    int32_t restart_counter = 0; // value will default to 0, if not set yet in NVS
-    err = nvs_get_i32(my_handle, "restart_conter", &restart_counter);
-    if (err != ESP_OK && err != ESP_ERR_NVS_NOT_FOUND) return err;
-
-    // Read run time blob
-    size_t required_size = 0;  // value will default to 0, if not set yet in NVS
+    // Read blob size
+    size_t required_size = 0;
     // obtain required memory space to store blob being read from NVS
     err = nvs_get_blob(my_handle, STORAGE_BLOB_1, NULL, &required_size);
     if (err != ESP_OK && err != ESP_ERR_NVS_NOT_FOUND) return err;
@@ -125,13 +100,6 @@ void app_main(void)
     }
     ESP_ERROR_CHECK( err );
 
-
-    // gpio_reset_pin(BOOT_MODE_PIN);
-    // gpio_set_direction(BOOT_MODE_PIN, GPIO_MODE_INPUT);
-
-    /* Read the status of GPIO0. If GPIO0 is LOW for longer than 1000 ms,
-       then save module's run time and restart it
-     */
     for (int i = 1; ; i++) {
         err = read_blob_size();
         ESP_LOGI(STORAGE_NAMESPACE, "Saving blob, pass %d", i);
